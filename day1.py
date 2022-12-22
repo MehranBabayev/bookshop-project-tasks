3eab9fc342093c5ae04858e2aa16a2a02aabd8eb
your_github_username <your_github_email>
2022-12-22 21:41:01 +0400

    day1

--- day1.py
+++ day1.py
@@ -1,1 +1,12 @@
-sfdsf
+import sys,datetime
+x = datetime.datetime.today().strftime("%d %B %Y")
+
+bookInfo = sys.stdin.readline().split(" - ")
+
+
+if len(bookInfo) != 2:
+    print("Wrong input")
+else:
+    print("Book name: " + bookInfo[0] + "\nWriter: " + bookInfo[1] + "Added in: " + x)
+
+
